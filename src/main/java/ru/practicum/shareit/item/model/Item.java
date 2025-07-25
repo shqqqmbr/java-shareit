6b590fea745db5929b249e8b2dca0d470e355b0b
berbenets_m <berbenets.maksim@bk.ru>
2025-07-25 10:12:46 +0300

    добавлены mapper`ы

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
+    @Positive
+    @NotNull
+    private int id;
+    private String name;
+    private String description;
+    private boolean available;
+    private User owner;
+    private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -3,5 +3,9 @@ package ru.practicum.shareit.user;
 /**
  * TODO Sprint add-controllers.
  */
+
 public class User {
+    private int id;
+    private String name;
+    private String email;
 }
